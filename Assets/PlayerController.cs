459cee9293c6290e89e24cfd3f4552219cd20644
SearWaizy <sear.waizy@student.forsbergsskola.se>
2022-05-28 20:38:47 +0200

    SpeedBooster Update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,17 +8,50 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D rb;
 
-    public float moveSpeed = 5;
+    //public float moveSpeed = 5;
+
+
+    private float speed;
+    private float boostTimer;
+    private bool boosting;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        speed = 5;
+        boostTimer = 0;
+        boosting = false;
     }
 
     void Update()
     {
         float moveDirection = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(0, moveDirection * moveSpeed);
+        rb.velocity = new Vector2(0, moveDirection * speed);
+
+
+        if (boosting)
+        {
+            boostTimer += Time.deltaTime;
+            if (boostTimer >= 3)
+            {
+                speed = 5;
+                boostTimer = 0;
+                boosting = false;
+            }
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SpeedBoost")
+        {
+            boosting = true;
+            speed = 10;
+            Destroy(other.gameObject);
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
